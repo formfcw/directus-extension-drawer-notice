da57c41a504b7e7a8ef8d90c02812b0cadc1f59f
formfcw <dev@formfcw.com>
2023-10-20 11:49:37 +0200

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Drawer Notice Interface for Directus
+
+[![NPM version](https://img.shields.io/npm/v/directus-extension-drawer-notice)](https://www.npmjs.com/package/directus-extension-drawer-notice)
+
+A notice field that is only visible in the drawer.
+
+```sh
+# Install via npm
+npm install directus-extension-drawer-notice
+# Install via yarn
+yarn add directus-extension-drawer-notice
+# Install via pnpm
+pnpm add directus-extension-drawer-notice
+```
+
+## Use Case
+
+The main use case for this extension is to display a message when editing a related item in the drawer, to prevent users from accidentally overwriting the contents of an existing item.
+
+Example:
+![](docs/use-case.png)
---
+++ src/interface.vue
@@ -0,0 +1,65 @@
+<template>
+    <div
+        ref="element"
+        class="drawer-notice"
+    >
+        <v-notice
+            :icon="icon"
+            :type="color"
+        >
+            <div v-md="text ?? t('interfaces.presentation-notice.no_text')" />
+        </v-notice>
+    </div>
+</template>
+
+
+
+<script setup lang="ts">
+    import { onMounted, ref } from 'vue';
+    import { useI18n } from 'vue-i18n';
+
+    const { t } = useI18n();
+
+    const props = withDefaults(
+        defineProps<{
+            color?: string;
+            icon?: string;
+            text?: string;
+            hidden: 'on_create' | 'on_update' | null;
+            primaryKey: string | number;
+        }>(),
+        {
+            color: 'normal',
+            icon: 'info',
+        }
+    );
+
+    const element = ref<HTMLElement | null>(null);
+
+    onMounted(() => {
+        const isCreating = props.primaryKey == '+';
+        const hiddenOnCreate = props.hidden == 'on_create';
+        const hiddenOnUpdate = props.hidden == 'on_update';
+        const hidden = !element.value?.closest(".v-drawer")
+            || (hiddenOnCreate && isCreating)
+            || (hiddenOnUpdate && !isCreating);
+        showField(hidden);
+    });
+
+    function showField(hidden: boolean = false): void {
+        const fieldElement: HTMLElement | null | undefined = element.value?.closest(".field");
+        if (!!fieldElement) fieldElement.hidden = hidden;
+    }
+</script>
+
+
+
+<style scoped>
+    :deep(a) {
+        color: var(--primary);
+    }
+
+    :deep(a:hover) {
+        color: var(--primary-125);
+    }
+</style>
